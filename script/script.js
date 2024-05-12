ed7a45f90be93db7e2a11afb52c0ba3beedb0cf4
Yassine <66925471+ymefath@users.noreply.github.com>
2024-05-12 16:55:36 +0100

    testing js and it is working
    try it

--- script/script.js
+++ script/script.js
@@ -6,15 +6,15 @@ fetch('Json/products.json')
         // Iterate over each product and generate HTML for product cards
         products.forEach(product => {
             const card = document.createElement('div');
-            card.classList.add('product-card', 'col-md-4', 'my-3', 'me-4'); // Adding me-3 for margin on the left side
+            card.classList.add('product-card', 'col-md-2' , 'my-2'); 
             card.innerHTML = `
-                <div class="card-container d-inline-flex flex-row flex-wrap justify-content-center gap-3">
-                    <div class="card" style="width: 18rem;">
+                <div class="card-container d-flex flex-row flex-wrap gap-1">
+                    <div class="card" style="width: 12rem;">
                         <a href="product.htm" style="text-decoration:none;">
                             <img src="${product.image}" class="card-img-top" alt="...">
-                            <div class="card-body d-inline-flex gap-3">
-                                <p class="card-text p-0 fs-6">${product.name}</p>
-                                <p class="card-text p-0 fw-bolder fs-2">${product.price}</p>
+                            <div class="card-body d-inline-block">
+                                <p class="card-text p-0 fs-6 m-0">${product.name}</p>
+                                <p class="card-text p-0 fw-bolder m-0 fs-4">${product.price}</p>
                             </div>
                         </a>
                     </div>
