4d5b9af1dfa92e4c6b1925f6bb4258c83bfc88be
Yassine <66925471+ymefath@users.noreply.github.com>
2024-05-12 14:19:53 +0100

    organisation

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-fetch('products.json')
+fetch('Json/products.json')
     .then(response => response.json())
     .then(products => {
         const container = document.getElementById('product-container');
