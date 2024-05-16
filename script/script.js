6d3c9dcd56bb35fdeec03a7269c903e49e2bf678
Yassine <66925471+ymefath@users.noreply.github.com>
2024-05-16 19:57:59 +0100

    improve main and frottement

--- script/script.js
+++ script/script.js
@@ -2,16 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('json/products.json')
         .then(response => response.json())
         .then(products => {
-            const productContainer = document.querySelector('.row-cols-md-6');
+            const productContainer = document.querySelector('.row.flex-row.flex-wrap');
 
             products.forEach(product => {
                 const productCard = `
-                    <div class="col">
+                    <div class="col mb-5">
                         <div class="card h-100">
                             <img src="${product.image}" class="card-img-top" alt="Product Image">
                             <div class="card-body">
-                                <h6 class="card-title">${product.name}</h6>
-                                <p class="card-text fs-59"><strong>Price:</strong> ${product.price}</p>
+                                <h6 class="card-title fs-5">${product.name}</h6>
+                                <p class="card-text fs-6"><strong>Price:</strong> ${product.price}</p>
                             </div>
                             <div class="card-footer p-0">
                                 <a href="pro.htm?name=${encodeURIComponent(product.name)}&price=${encodeURIComponent(product.price)}&description=${encodeURIComponent(product.description)}&image=${encodeURIComponent(product.image)}" 
